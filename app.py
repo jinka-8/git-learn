fc84bebcfe34c3cdab2d0c0564f75379b67742af
jinka-8 <131753656+jinka-8@users.noreply.github.com>
2023-12-06 12:11:33 +0530

    Create app.py

---
+++ app.py
@@ -0,0 +1,15 @@
+from flask import Flask
+import redis
+
+app = Flask(__name__)
+redis_client = redis.StrictRedis(host='localhost', port=6379, decode_responses=True)
+
+@app.route('/')
+def hello():
+    redis_client.incr('hits')
+    counter = str(redis_client.get('hits'))
+    
+    return "Welcome to this webpage! This webpage has been viewed " + counter + " time(s)"
+
+if __name__ == '__main__':
+    app.run(host='0.0.0.0', debug=True)
