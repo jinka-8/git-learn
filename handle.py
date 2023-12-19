7286a31f786bf3c8e0fb4524f5d6dd239c76aa4f
Kumar Swamy Jinka <k.swamy@klizos.com>
2023-12-19 20:08:07 +0530

    checking purpose

--- handle.py
+++ handle.py
@@ -4,7 +4,7 @@ def get_contacts_count(url):
     response = requests.get(url)
     if response.status_code == 200:
         response_data = response.json()
-        #merge for testing
+        #merge for tesing2
         number_of_contacts = response_data.get('data',{}).get('total',0)
 
         return number_of_contacts
